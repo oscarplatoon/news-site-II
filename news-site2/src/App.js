4067781c211b64c53fbdc3f4094c94b0e1912209
Jeremiah Mauga <miahmauga@gmail.com>
2021-07-22 14:17:51 -0700

    Jeremiah and James news-site-2

---
+++ news-site2/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import News from './data/news.json'
+import navItems from './data/navItems.json';
+import './App.css';
+import AppNav from './components/AppNav/AppNav.js';
+import HomePage from './pages/HomePage';
+import ArticlePage from './pages/ArticlePage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const randomArticleIndex = Math.floor(Math.random() * News.length);
+    const randomArticle = News[randomArticleIndex];
+
+    this.state = {
+      navItems: navItems,
+      article: {
+        id: randomArticleIndex,
+        title: randomArticle.title,
+        abstract: randomArticle.abstract,
+        byline: randomArticle.byline,
+        image: randomArticle.multimedia.length ? randomArticle.multimedia[0].url : null,
+        created_date: randomArticle.created_date
+      }
+    }
+  }
+
+  greeting = () => {
+    console.log("Greeting")
+  }
+
+
+  // render() {
+    // const { id, title, created_date, handleTitleClick } = this.state
+
+  //   return (
+  //     <div>
+
+  //       <ArticleList
+  //       articles={News}
+  //       handleTitleClick={() => this.greeting()}
+  //       />
+        
+  //     </div>
+  //   );
+  // }
+  render() {
+    const { navItems } = this.state
+  
+    return (
+      <div>
+        <h1>AppNav Component</h1>
+        <hr />
+        <AppNav navItems={navItems} handleNavClick={(clickedItem) => console.log(clickedItem)} />
+        <Router>
+          <div>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/articles/:articleID" component={ArticlePage} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
