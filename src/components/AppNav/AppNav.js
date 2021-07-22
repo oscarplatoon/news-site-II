b6f97d797ef501fa8a1f2d78a34e84c7c6552661
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-22 17:21:27 -0400

    Working Stylized App Nav Bar

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Nav, Navbar} from 'react-bootstrap';
+
 
 class AppNav extends Component {
+
+  renderNavItems = (navItems) => {
+    let elements = navItems.map((navItem, index) => {
+      return (
+        <Nav.Link href="#" key={ index } onClick={ () => this.handleNavClick(navItem.value)}  >
+          | {navItem.label} |
+        </Nav.Link>
+      )
+    })
+    return (
+      <Nav>
+        { elements }
+      </Nav>
+    )
+  }
+
   render() {
     const { navItems, handleNavClick } = this.props;
 
     return (
-      <Navbar className='navbar-dark'>
-        {navItems.map((navItem, index) => {
-          <a href="#" key={ index } onClick={ () => handleNavClick(navItem.value)} >
-           | {navItem.label} |
-          </a>
-        }
-        )}
+      <Navbar bg='dark' variant='dark'>
+        { this.renderNavItems(navItems) }
       </Navbar>
     )
   }
